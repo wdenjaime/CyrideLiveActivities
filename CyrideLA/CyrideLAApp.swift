bfc1e76dfdf948972d4a6b1f6fe6a2011e2462ff
wdenjaime <wdenjaime@iastate.edu>
2025-03-12 12:29:48 -0500

    Initial Commit

---
+++ CyrideLA/CyrideLAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyrideLAApp.swift
+//  CyrideLA
+//
+//  Created by Jaime, Edgar N [ITS] on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CyrideLAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
